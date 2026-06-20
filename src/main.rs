346ab9d4d7aa2db977d48ed2c98d40bd121f00ab
CANUreal <dnzdyn2012@gmail.com>
2026-06-20 20:07:37 +0300

    1st web scraper with no seperating any USER_AGENT and ONLY scrapes html no js.

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use reqwest::Url;
+use scraper::{Html, Selector};
+use tokio::io::{self, AsyncBufReadExt, BufReader, AsyncWriteExt};
+
+
+async fn palantir() -> String {
+    let mut stdout = io::stdout();
+    let _ = stdout.write_all(b"can you spare some url siir?  ").await;
+    let _ = stdout.flush().await;
+    
+    let mut id = String::new();
+    let stdin = io::stdin();
+    let mut reader = BufReader::new(stdin);
+
+    reader
+        .read_line(&mut id)
+        .await
+        .expect("Couldn2t read it braj");
+
+    let mut input = id.trim().to_string();
+    if !input.starts_with("http://") && !input.starts_with("https://") && !input.is_empty() {
+        input = format!("https://{}", input);
+    }
+    input
+}
+
+async fn get_data_from_address(url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    println!("scraping the url, : {}", url);
+
+    let parsedd_url = Url::parse(url)?;
+
+    let response = reqwest::get(parsedd_url).await?.text().await?;
+    let document = Html::parse_document(&response);
+    let h1_slctr = Selector::parse("p").map_err(|e| format!("Inavlid selector {:?}", e))?;
+
+    for element in document.select(&h1_slctr) {
+        let text = element.text().collect::<Vec<_>>().join( " ");
+        println!("> {}", text.trim());
+    }
+    Ok(())
+}
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let url = palantir().await;
+    
+    if url.is_empty() {
+        println!("invalid input");
+        return Ok(())
+    }
+
+    if let Err(e) = get_data_from_address(&url).await {
+        eprintln!("An error occured {}", e);
+    }
+
+    let mut stdout = io::stdout();
+
+    stdout.write_all(b"This was all.\n").await?;
+
+    stdout.flush().await?;
+    
+    stdout.write_all(b"Press RET to end it").await?;
+    let _ = stdout.flush().await?;
+
+    let stdin = io::stdin();
+    let mut reader = BufReader::new(stdin);
+    let mut buf = String::new(); 
+
+    reader.read_line(&mut buf).await?;
+    
+    Ok(())
+}
